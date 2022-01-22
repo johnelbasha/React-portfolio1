b1eaea55a7c2d7823634042b154a5bbd34414c82
John El Basha <traillaptop@Johns-MacBook-Pro.local>
2022-01-22 22:00:32 +0000

    Added new component which has nested components.

--- src/index.js
+++ src/index.js
@@ -2,40 +2,57 @@ import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
 import faker from "faker";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45 PM"
-        text="This is a great idea."
-        imageURL={faker.image.image()}
-      />
-      <CommentDetail
-        author="Thomas"
-        timeAgo="Today at 1:45 PM"
-        text="Why haven't we tried this before?"
-        imageURL={faker.image.image()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Yesterday at 2:45 PM"
-        text="Has this been proven in a laboratory setting?"
-        imageURL={faker.image.image()}
-      />
-      <CommentDetail
-        author="Eric"
-        timeAgo="Yesterday at 3:22 PM"
-        text="This is crazy"
-        imageURL={faker.image.image()}
-      />
-      <CommentDetail
-        author="Peter"
-        timeAgo="5 minutes ago"
-        text="I tried it, but it didn't work for me."
-        imageURL={faker.image.image()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45 PM"
+          text="This is a great idea."
+          imageURL={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Thomas"
+          timeAgo="Today at 1:45 PM"
+          text="Why haven't we tried this before?"
+          imageURL={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 2:45 PM"
+          text="Has this been proven in a laboratory setting?"
+          imageURL={faker.image.image()}
+          />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Eric"
+          timeAgo="Yesterday at 3:22 PM"
+          text="This is crazy"
+          imageURL={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Peter"
+          timeAgo="5 minutes ago"
+          text="I tried it, but it didn't work for me."
+          imageURL={faker.image.image()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
